3b52a01886c005a1f1e1b61a2860fc30ece7326a
Samrux <samruxb@gmail.com>
2018-04-15 20:27:25 -0300

    publish

--- Publish.bat
+++ Publish.bat
@@ -6,7 +6,7 @@ dotnet publish PacManBot.csproj --runtime %RUNTIME% --configuration Release
 move /Y "bin/netcoreapp2.0/%RUNTIME%/publish" "bin/"
 rd /s /q "bin/%RUNTIME%"
 move /Y "bin/publish" "bin/%RUNTIME%"
-robocopy "bin" "bin/%RUNTIME%" *.bot /IS /XF config.bot
+robocopy "bin" "bin/%RUNTIME%" *.bot /IS
 
 set RUNTIME=linux-x64
 
