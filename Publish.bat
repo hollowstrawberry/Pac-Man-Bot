453fc3abf8237be3f1ae2df3d79e43094706ec90
Samrux <samruxb@gmail.com>
2018-04-20 12:41:00 -0300

    Testing

--- Publish.bat
+++ Publish.bat
@@ -1,6 +1,4 @@
 
-rmdir /s /q "bin/Release"
-
 set RUNTIME=win-x86
 dotnet publish PacManBot.csproj --runtime %RUNTIME% --configuration Release
 copy "bin\contents.bot" "bin/Release/netcoreapp2.0/%RUNTIME%/publish/contents.bot"
