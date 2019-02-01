fda5a6b81fe02566c008f28879f6c581465a22b1
Samuel <33796679+Samrux@users.noreply.github.com>
2019-02-01 15:20:04 -0300

    new prefix checking functions?

--- src/Services/PmDiscordClient.cs
+++ src/Services/PmDiscordClient.cs
@@ -1,4 +1,6 @@
-﻿using Discord.WebSocket;
+﻿using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Discord.WebSocket;
 
 namespace PacManBot.Services
 {
@@ -7,6 +9,19 @@ namespace PacManBot.Services
     /// </summary>
     public class PmDiscordClient : DiscordShardedClient
     {
-        public PmDiscordClient(PmConfig config) : base(config.ClientConfig) { }
+        /// <summary>Is a match when the given text begins with a mention to the bot's current user.</summary>
+        public Regex MentionPrefix { get; private set; }
+
+        public PmDiscordClient(PmConfig config) : base(config.ClientConfig)
+        {
+            ShardReady += OnShardReady;
+        }
+
+        private Task OnShardReady(DiscordSocketClient arg)
+        {
+            MentionPrefix = new Regex($@"^<@!?{CurrentUser.Id}>");
+            ShardReady -= OnShardReady;
+            return Task.CompletedTask;
+        }
     }
 }
