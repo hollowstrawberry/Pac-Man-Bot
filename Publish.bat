d78645c3ff68bd72375c67c31fd53048014874f3
Samuel <33796679+orchidalloy@users.noreply.github.com>
2020-03-09 15:09:38 -0300

    arm

--- Publish.bat
+++ Publish.bat
@@ -1,9 +1,9 @@
 echo off
 
 ::You can change the runtime to win-x64 for Windows, linux-arm for a Raspberry Pi, etc.
-set RUNTIME=linux-x64
+set RUNTIME=linux-arm
 dotnet publish PacManBot.csproj --runtime %RUNTIME% --configuration Release --self-contained
-copy "bin\contents.json" "bin\Release\netcoreapp2.0\%RUNTIME%\publish\contents.json"
+::copy "bin\contents.json" "bin\Release\netcoreapp2.0\%RUNTIME%\publish\contents.json"
 
 ::del /Q "bin\Release\netcoreapp2.0\%RUNTIME%\*"
 
